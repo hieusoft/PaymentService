5ed479b41fd6059789fbe33bfbc5eda76a413822
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-11-26 21:04:27 +0700

    a

--- Models/Payment/InvoiceResponse.cs
+++ Models/Payment/InvoiceResponse.cs
@@ -1,18 +1,76 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using System.Web;
 using PaymentService.Models.Entities;
 
 namespace PaymentService.Models.Payment
 {
     public interface IInvoiceResponse
     {
-        public string this[string key] { get; }
+        public string? this[string key] { get; }
     }
 
-    public class URLInvoiceResponse : IInvoiceResponse
+    public class UrlQueryInvoiceResponse : IInvoiceResponse
     {
-        public string this[string key] => throw new NotImplementedException();
+        readonly NameValueCollection keys;
+        
+        public string? this[string key] { get { return keys[key]; } }
+
+        public UrlQueryInvoiceResponse (string url)
+        {
+            Uri uriObject = new (url);
+            keys = new(HttpUtility.ParseQueryString(uriObject.Query));
+        }
+    }
+
+    public class JsonInvoiceResponse : IInvoiceResponse
+    {
+        readonly NameValueCollection keys = new();
+        
+        public string? this[string key] { get { return keys[key]; } }
+
+        public JsonInvoiceResponse (string json)
+        {
+            JsonDocument jsonDoc = JsonDocument.Parse(json);
+
+            void ProcessJsonElement(JsonElement element, string prefix)
+            {
+                switch (element.ValueKind)
+                {
+                    case JsonValueKind.Number:
+                        // trim last period character 
+                        keys[prefix[..^1]] = element.GetDouble().ToString();
+                        return;
+                    case JsonValueKind.String:
+                        // trim last period character 
+                        keys[prefix[..^1]] = element.GetString();
+                        return;
+                    case JsonValueKind.Object:
+                        foreach (var child in element.EnumerateObject())
+                        {
+                            ProcessJsonElement(child.Value, prefix + child.Name + ".");
+                        }
+                        return;
+                    case JsonValueKind.Array:
+                        int index = 0;
+                        foreach (var child in element.EnumerateArray())
+                        {
+                            ProcessJsonElement(child, prefix + index + ".");
+                            index++;
+                        }
+                        keys[prefix + "Length"] = index.ToString();
+                        return;
+                    default:
+                        return;
+                }
+            }
+            ProcessJsonElement(jsonDoc.RootElement, ".");
+        }
     }
 }
